bba34d7fd9f21ef76fb4695026119e439aaf85b6
Nauruz Guliev <Nauruz9248327@gmail.com>
2024-04-14 20:26:47 +0300

    failure class

---
+++ src/test/java/ru/kpfu/itis/gnt/helper/ClassWithErrors.java
@@ -0,0 +1,14 @@
+package ru.kpfu.itis.gnt.helper;
+
+import org.junit.Test;
+
+public class ClassWithErrors {
+
+    @Test
+    public void testLogout() {
+
+
+
+        System.out.println("FAILED TEST");
+    }
+}
